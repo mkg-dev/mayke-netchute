9c50871cc47b637f2e521b452561244536dad8db
Mayke Carvalho <75760701+mkg-dev@users.noreply.github.com>
2022-03-30 20:48:46 -0300

    adicionando arquivos

---
+++ banco.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clientes(
+    codigo int primary key,
+    nome varchar(255) NOT NULL,
+    scpf varchar(99) NOT NULL,
+    email varchar(255) NOT NULL
+);
+
+
+
+
+
---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b9ce4f289118e9', 'f7bf308c', 'heroku_c7aec4866498ddd') or die('Erro ao conectar ao banco de dados');
